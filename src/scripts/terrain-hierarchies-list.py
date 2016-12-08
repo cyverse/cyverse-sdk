fa131072a283e5217e34389303d66bb1e7f2b7a6
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-12-08 15:39:34 -0600

    split into two scripts

--- src/scripts/terrain-hierarchies-list.py
+++ src/scripts/terrain-hierarchies-list.py
@@ -5,15 +5,13 @@ import argparse
 import json
 import os.path
 import requests
-import urllib
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 if __name__ == '__main__':
 
     # arguments
-    parser = argparse.ArgumentParser(description = 'List apps under hierarchy provided.')
-    parser.add_argument('-i', '--iri', dest = 'iri', nargs = '?', help = 'hierarchy url for which to list apps')
+    parser = argparse.ArgumentParser(description = 'List hierarchys in nested format')
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true', help = 'verbose output')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', nargs = '?', help = 'access token')
     args = parser.parse_args()
@@ -25,20 +23,15 @@ if __name__ == '__main__':
     # build header
     header = {'Authorization': 'Bearer ' + args.accesstoken}
 
-    # if only -i given
-    if args.iri is None:
-        args.iri = terrainpy.prompt_user('iri')
-    args.iri = '/' + urllib.quote(args.iri, '') + '/apps?attr=rdf%3Atype'
-
     # list hierarchys
-    url = 'https://agave.iplantc.org/terrain/v2/apps/hierarchies' + args.iri
-    list_apps = requests.get(url, headers = header, verify = False)
-    list_apps.raise_for_status
-    list_apps = list_apps.json()
+    url = 'https://agave.iplantc.org/terrain/v2/apps/hierarchies'
+    list_hierarchys = requests.get(url, headers = header, verify = False)
+    list_hierarchys.raise_for_status
+    list_hierarchys = list_hierarchys.json()
 
     # print output based on -v flag
     if args.verbose:
-	print json.dumps(list_apps, sort_keys = True, indent = 4, separators = (',', ': '))
+	print json.dumps(list_hierarchys, sort_keys = True, indent = 4, separators = (',', ': '))
     else:
-	for item in list_apps['apps']:
-	    print item['name'], '\t', item['id']
+	for item in list_hierarchys['hierarchies']:
+	    print item['label'], '\t', item['iri']
