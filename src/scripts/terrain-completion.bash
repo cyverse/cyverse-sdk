0dac53da6fb1cb31db136f9965e34628a766a8c9
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-12-09 16:33:17 -0600

    bashcomplete update 1

--- src/scripts/terrain-completion.bash
+++ src/scripts/terrain-completion.bash
@@ -6,12 +6,10 @@ _terrain() {
     cur=${COMP_WORDS[COMP_CWORD]}
     prev=${COMP_WORDS[COMP_CWORD-1]}
 
-    if [ $COMP_CWORD -eq 0 ]; then
-	echo COMP_CWORD equals 0
+    if [ $COMP_CWORD -eq 1 ]; then
         COMPREPLY=( $(compgen -W "apps hierarchies jobs" -- $cur) )
     
-    elif [ $COMP_CWORD -eq 1 ]; then
-        echo COMP_CWORD equals 1
+    elif [ $COMP_CWORD -eq 2 ]; then
         case "$prev" in
 	    app|apps)		   COMPREPLY=( $(compgen -W "description list search" -- $cur) ) ;;
 	    hierarchy|hierarchies) COMPREPLY=( $(compgen -W "list" -- $cur) ) ;;
@@ -20,7 +18,6 @@ _terrain() {
 	esac
 
     elif [ $COMP_CWORD -gt 2 ]; then
-        echo COMP_CWORD equals 2
 	case "$prev" in
 	    -f|--description_file)	COMPREPLY=( $(compgen -f $cur)) ;;
 	    *)				;;
@@ -28,4 +25,5 @@ _terrain() {
     fi
 }
 
-complete -F _terrain terrain-apps-description.py terrain-apps-list-by-hierarchy.py terrain-apps-search.py terrain-hierarchies-list.py terrain-jobs-submit.py terrain-jobs-template.py
+#complete -F _terrain terrain-apps-description.py terrain-apps-list-by-hierarchy.py terrain-apps-search.py terrain-hierarchies-list.py terrain-jobs-submit.py terrain-jobs-template.py
+complete -F _terrain terrain
