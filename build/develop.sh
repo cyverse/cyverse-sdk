d7051e7edaafeda95cae377c66aabd2f06b68bd2
Matthew Vaughn <vaughn@tacc.utexas.edu>
2017-06-20 14:57:16 -0500

    Just build container for now with develop.sh

--- build/develop.sh
+++ build/develop.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 cp VERSION VERSION.bak
-echo "edge" > VERSION
+echo "develop" > VERSION
 
 _VERSION="$(echo -n $(cat VERSION))"
 make clean
-make dist
-make clean
-
-git commit -a -m "Building release ${_VERSION}"
-git tag -a "v${_VERSION}" -m "version ${_VERSION}"
-git push origin "v${_VERSION}"
-git push origin master
 
 make docker
 make docker-release
